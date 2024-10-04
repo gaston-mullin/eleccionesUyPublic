3fa46d75a04f853b10bef2b904554745f441bffb
Gastón Mullin <g.mullin@tilburguniversity.edu>
2024-10-04 23:01:59 +0200

    Quitar resource

--- data/websiteResources/Montevideo perdió dos bancas; una ganó Canelones y otra Tacuarembó - Noticias Uruguay, LARED21 Diario Digital_files/ajs.php
+++
@@ -1,3 +0,0 @@
-var OX_d0aeb09a = '';
-OX_d0aeb09a += "<"+"div id=\'beacon_0882a10a08\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'><"+"img src=\'https://servedby.revive-adserver.net/lg.php?bannerid=0&amp;campaignid=0&amp;zoneid=7846&amp;loc=https%3A%2F%2Fwww.lr21.com.uy%2Fpolitica%2F157429-montevideo-perdio-dos-bancas-una-gano-canelones-y-otra-tacuarembo&amp;referer=https%3A%2F%2Fwww.google.com%2F&amp;cb=0882a10a08\' width=\'0\' height=\'0\' alt=\'\' style=\'width: 0px; height: 0px;\' /><"+"/div>\n";
-document.write(OX_d0aeb09a);
